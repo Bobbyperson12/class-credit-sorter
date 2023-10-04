bb070a88274fc9ecda772dea024779c6aa687c63
Bobbyperson12 <bobbyperson12345@gmai.com>
2023-10-04 16:14:13 -0400

    json through post requests

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pathlib import Path
-import time
-import os
 import json
 
 data_folder = Path("backend/data/") # TODO: Will cause errors if not run from main folder, needs a fix.
@@ -14,8 +12,13 @@ class MainServer(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
-        self.send_header("Access-Control-Allow-Origin", "*")    # This is a security issue. Essentially this controls who can access the server from what website. 
-        self.end_headers()                                      # So * means anyone can access it. In a real environment this needs to be changed.
+        self.send_header("Access-Control-Allow-Origin", "*") # needs to be changed off of * in a prod environment
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")  # Allow these methods
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")  # Allow Content-Type header
+        self.end_headers()
+
+    def do_OPTIONS(self): # inform browser of what methods are allowed
+        self._set_headers()
 
         
     # GET sends back the required classes for the degree
@@ -24,7 +27,7 @@ class MainServer(BaseHTTPRequestHandler):
         degree = self.path[1:] # Remove the beginning slash
         file_lines = self.get_required_classes(degree)
 
-        if file_lines is None:
+        if file_lines == []:
             degree_message = None,
             required_classes = None,
             error = True, 
@@ -34,6 +37,40 @@ class MainServer(BaseHTTPRequestHandler):
             required_classes = file_lines
             error = False
             error_message = None
+        response = {
+            "degree": degree_message,
+            "required_classes": required_classes,
+            "error": error,
+            "error_message": error_message
+        }
+
+        self.wfile.write(bytes(json.dumps(response), "utf-8"))
+        
+    def do_POST(self):
+        self._set_headers()
+
+        # Extract and parse POST data
+        content_length = int(self.headers['Content-Length'])  # Get the size of data
+        post_data = self.rfile.read(content_length)  # Get the data
+        parsed_data = json.loads(post_data)  # Parse the JSON data
+
+        # Extract the required data
+        degree = parsed_data.get("degree", "")
+        # other_info = parsed_data.get("other_info")  # If you need other info
+
+        file_lines = self.get_required_classes(degree)
+
+        if file_lines == []:
+            degree_message = None
+            required_classes = None
+            error = True
+            error_message = "Degree not found."
+        else:
+            degree_message = self.get_full_degree_name(degree)
+            required_classes = file_lines
+            error = False
+            error_message = None
+
         response = {
             "degree": degree_message,
             "required_classes": required_classes,
--- index.js
+++ index.js
@@ -2,3 +2,27 @@
 const test = () => {
   console.log('Hello World!');
 }
+
+// Define the data
+const data = {
+  degree: "CS-Major",
+  other_info: 123
+};
+
+// Make the POST 
+const posttest = () => {
+  fetch("http://localhost:8080/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)  // Convert the data object to a JSON string
+  })
+    .then(response => response.json())  // Parse the response as JSON
+    .then(data => {
+      console.log(data);  // Log the parsed response data
+    })
+    .catch(error => {
+      console.error("Error:", error);  // Log any errors
+    });
+}
